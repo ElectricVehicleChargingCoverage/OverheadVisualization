868270b0c53be4e7c1f22cbd3c7ffb422835a2df
JulianEgbert <Julian.Egbert@student.hpi.de>
2022-01-14 14:17:43 +0100

    Initialize Map

---
+++ map.js
@@ -0,0 +1,6 @@
+var map = L.map("map").setView([51.163361, 10.447683], 7);
+
+L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+    attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
