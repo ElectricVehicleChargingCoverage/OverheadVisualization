d55509fcb5799f8f594747646b9f46f7110962cf
JulianEgbert <Julian.Egbert@student.hpi.de>
2022-01-14 14:58:18 +0100

    Implement version with GeoJSON

--- map.js
+++ map.js
@@ -4,3 +4,16 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
     attribution:
         '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
 }).addTo(map);
+
+const style = {
+    color: "#ff0000",
+};
+L.geoJSON(cityData, {
+    style: style,
+    pointToLayer: function (feature, latlng) {
+        console.log(feature);
+        return L.circleMarker(latlng, {
+            radius: 80 * (feature.properties.Zeitfaktor -1),
+        });
+    },
+}).addTo(map);
