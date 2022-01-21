617c52c89cc79b398db137ab0e4e819c30d5479d
JulianEgbert <Julian.Egbert@student.hpi.de>
2022-01-21 11:12:19 +0100

    Getting an API response

--- calculateRoutes.js
+++ calculateRoutes.js
@@ -1,2 +1,9 @@
-const url =
-    "https://api.tomtom.com/routing/1/calculateRoute/52.50931%2C13.42936%3A52.50274%2C13.43872/json?maxAlternatives=0&language=de-DE&computeBestOrder=false&routeRepresentation=polyline&computeTravelTimeFor=all&sectionType=travelMode&departAt=now&routeType=fastest&traffic=false&avoid=unpavedRoads&travelMode=car&vehicleMaxSpeed=0&vehicleWeight=0&vehicleAxleWeight=0&vehicleLength=0&vehicleWidth=0&vehicleHeight=0&vehicleCommercial=false&vehicleEngineType=combustion&key=****";
+const key = "???";
+const url = `https://api.tomtom.com/routing/1/calculateRoute/52.50931%2C13.42936%3A52.50274%2C13.43872/json?maxAlternatives=0&language=de-DE&computeBestOrder=false&routeRepresentation=polyline&computeTravelTimeFor=all&sectionType=travelMode&departAt=now&routeType=fastest&traffic=false&avoid=unpavedRoads&travelMode=car&vehicleMaxSpeed=0&vehicleWeight=0&vehicleAxleWeight=0&vehicleLength=0&vehicleWidth=0&vehicleHeight=0&vehicleCommercial=false&vehicleEngineType=combustion&key=${key}`;
+
+httpGetAsync(url, displayAPIResult);
+
+function displayAPIResult(response) {
+    const json = JSON.parse(response);
+    console.log(json);
+}
