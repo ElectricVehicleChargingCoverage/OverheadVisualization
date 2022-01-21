c61f37de576f0f3a97de6158e1bd8bfa3a234b10
JulianEgbert <Julian.Egbert@student.hpi.de>
2022-01-21 11:05:07 +0100

    implement basis for route calculation

---
+++ calculateRoutes.js
@@ -0,0 +1,2 @@
+const url =
+    "https://api.tomtom.com/routing/1/calculateRoute/52.50931%2C13.42936%3A52.50274%2C13.43872/json?maxAlternatives=0&language=de-DE&computeBestOrder=false&routeRepresentation=polyline&computeTravelTimeFor=all&sectionType=travelMode&departAt=now&routeType=fastest&traffic=false&avoid=unpavedRoads&travelMode=car&vehicleMaxSpeed=0&vehicleWeight=0&vehicleAxleWeight=0&vehicleLength=0&vehicleWidth=0&vehicleHeight=0&vehicleCommercial=false&vehicleEngineType=combustion&key=****";
